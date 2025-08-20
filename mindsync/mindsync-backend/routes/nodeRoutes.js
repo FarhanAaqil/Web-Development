97fd9ae7f20af3727c41043e9f0bce157d10891b
FarhanAaqil <fadurrani543@gmail.com>
2025-08-21 02:19:42 +0530

    Delete mindsync directory

--- mindsync/mindsync-backend/routes/nodeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const nodeController = require('../controllers/nodeController');
-const router = express.Router();
-
-router.post('/', nodeController.createNode);
-router.post('/link', nodeController.linkNodes);
-router.get('/:userId', nodeController.getUserNodes);
-
-module.exports = router;
